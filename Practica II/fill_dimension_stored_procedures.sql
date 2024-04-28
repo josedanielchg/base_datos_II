e9bbbbd064b5cd3cac162aa1bdacf3118bda1e3d
Pedro Soto <pedrosotto717@gmail.com>
2024-04-28 16:05:41 -0400

    code indentation

--- Practica II/fill_dimension_stored_procedures.sql
+++ Practica II/fill_dimension_stored_procedures.sql
@@ -15,14 +15,20 @@ BEGIN
     IF (SELECT COUNT(*) FROM DimTime) > 0 THEN
         -- Llenar DimTime solo con datos del día actual si ya hay datos y no existe un registro para ese día
         IF NOT EXISTS (SELECT 1 FROM DimTime WHERE Date = CURDATE()) THEN
-            SET @sql = CONCAT('INSERT INTO DimTime (Date, Day, Month, Year, IsWeekend) SELECT DISTINCT CheckinDate, DAY(CheckinDate), MONTH(CheckinDate), YEAR(CheckinDate), CASE WHEN DAYOFWEEK(CheckinDate) IN (1, 7) THEN TRUE ELSE FALSE END FROM `', dbName, '`.Booking WHERE CheckinDate = CURDATE() ON DUPLICATE KEY UPDATE Day = VALUES(Day), Month = VALUES(Month), Year = VALUES(Year), IsWeekend = VALUES(IsWeekend)');
+            SET @sql = CONCAT('INSERT INTO DimTime (Date, Day, Month, Year, IsWeekend) \
+                SELECT DISTINCT CheckinDate, DAY(CheckinDate), MONTH(CheckinDate), YEAR(CheckinDate), CASE WHEN DAYOFWEEK(CheckinDate) \
+                IN (1, 7) THEN TRUE ELSE FALSE END FROM `', dbName, '`.Booking WHERE CheckinDate = CURDATE() \
+                ON DUPLICATE KEY UPDATE Day = VALUES(Day), Month = VALUES(Month), Year = VALUES(Year), IsWeekend = VALUES(IsWeekend)');
             PREPARE stmt FROM @sql;
             EXECUTE stmt;
             DEALLOCATE PREPARE stmt;
         END IF;
     ELSE
         -- Llenar DimTime con todos los datos si es la primera vez
-        SET @sql = CONCAT('INSERT INTO DimTime (Date, Day, Month, Year, IsWeekend) SELECT DISTINCT CheckinDate, DAY(CheckinDate), MONTH(CheckinDate), YEAR(CheckinDate), CASE WHEN DAYOFWEEK(CheckinDate) IN (1, 7) THEN TRUE ELSE FALSE END FROM `', dbName, '`.Booking ON DUPLICATE KEY UPDATE Day = VALUES(Day), Month = VALUES(Month), Year = VALUES(Year), IsWeekend = VALUES(IsWeekend)');
+        SET @sql = CONCAT('INSERT INTO DimTime (Date, Day, Month, Year, IsWeekend) \
+                SELECT DISTINCT CheckinDate, DAY(CheckinDate), MONTH(CheckinDate), YEAR(CheckinDate), CASE WHEN DAYOFWEEK(CheckinDate) IN (1, 7) \
+                THEN TRUE ELSE FALSE END FROM `', dbName, '`.Booking ON DUPLICATE KEY \
+                UPDATE Day = VALUES(Day), Month = VALUES(Month), Year = VALUES(Year), IsWeekend = VALUES(IsWeekend)');
         PREPARE stmt FROM @sql;
         EXECUTE stmt;
         DEALLOCATE PREPARE stmt;
@@ -37,13 +43,15 @@ BEGIN
     END IF;
 
     -- Llenar DimRoomType solo con tipos de habitación nuevos
-    SET @sql = CONCAT('INSERT INTO DimRoomType (Type) SELECT DISTINCT Name FROM `', dbName, '`.RoomType WHERE Name NOT IN (SELECT Type FROM DimRoomType) ON DUPLICATE KEY UPDATE Type = VALUES(Type)');
+    SET @sql = CONCAT('INSERT INTO DimRoomType (Type) SELECT DISTINCT Name FROM `', dbName, '`.RoomType WHERE Name NOT IN \
+            (SELECT Type FROM DimRoomType) ON DUPLICATE KEY UPDATE Type = VALUES(Type)');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
     -- Llenar DimHotel solo con hoteles nuevos
-    SET @sql = CONCAT('INSERT INTO DimHotel (HotelID, HotelName) SELECT ID, Name FROM `', dbName, '`.Hotel WHERE ID NOT IN (SELECT HotelID FROM DimHotel) ON DUPLICATE KEY UPDATE HotelName = VALUES(HotelName)');
+    SET @sql = CONCAT('INSERT INTO DimHotel (HotelID, HotelName) SELECT ID, Name FROM `', dbName, '`.Hotel WHERE ID NOT IN \
+            (SELECT HotelID FROM DimHotel) ON DUPLICATE KEY UPDATE HotelName = VALUES(HotelName)');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
